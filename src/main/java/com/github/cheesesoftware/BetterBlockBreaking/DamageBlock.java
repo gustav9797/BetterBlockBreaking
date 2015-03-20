c863c7668a749cce871b26989c1962205c48dc61
Gustav Hammar <gustav9797@hotmail.se>
2015-03-20 18:07:45 +0100

    Update to 1.8.3

--- src/main/java/com/github/cheesesoftware/BetterBlockBreaking/DamageBlock.java
+++ src/main/java/com/github/cheesesoftware/BetterBlockBreaking/DamageBlock.java
@@ -2,22 +2,22 @@ package com.github.cheesesoftware.BetterBlockBreaking;
 
 import java.util.Date;
 
-import net.minecraft.server.v1_8_R1.BlockPosition;
-import net.minecraft.server.v1_8_R1.EntityChicken;
-import net.minecraft.server.v1_8_R1.EntityLiving;
-import net.minecraft.server.v1_8_R1.PacketPlayOutBlockBreakAnimation;
-import net.minecraft.server.v1_8_R1.PacketPlayOutBlockChange;
-import net.minecraft.server.v1_8_R1.TileEntity;
-import net.minecraft.server.v1_8_R1.WorldServer;
+import net.minecraft.server.v1_8_R2.BlockPosition;
+import net.minecraft.server.v1_8_R2.EntityChicken;
+import net.minecraft.server.v1_8_R2.EntityLiving;
+import net.minecraft.server.v1_8_R2.PacketPlayOutBlockBreakAnimation;
+import net.minecraft.server.v1_8_R2.PacketPlayOutBlockChange;
+import net.minecraft.server.v1_8_R2.TileEntity;
+import net.minecraft.server.v1_8_R2.WorldServer;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
-import org.bukkit.craftbukkit.v1_8_R1.CraftServer;
-import org.bukkit.craftbukkit.v1_8_R1.CraftWorld;
-import org.bukkit.craftbukkit.v1_8_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_8_R2.CraftServer;
+import org.bukkit.craftbukkit.v1_8_R2.CraftWorld;
+import org.bukkit.craftbukkit.v1_8_R2.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
--- src/main/java/com/github/cheesesoftware/BetterBlockBreaking/RemoveOldDamagedBlocksTask.java
+++ src/main/java/com/github/cheesesoftware/BetterBlockBreaking/RemoveOldDamagedBlocksTask.java
@@ -6,14 +6,14 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import net.minecraft.server.v1_8_R1.BlockPosition;
-import net.minecraft.server.v1_8_R1.PacketPlayOutBlockBreakAnimation;
-import net.minecraft.server.v1_8_R1.WorldServer;
+import net.minecraft.server.v1_8_R2.BlockPosition;
+import net.minecraft.server.v1_8_R2.PacketPlayOutBlockBreakAnimation;
+import net.minecraft.server.v1_8_R2.WorldServer;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_8_R1.CraftServer;
-import org.bukkit.craftbukkit.v1_8_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_8_R2.CraftServer;
+import org.bukkit.craftbukkit.v1_8_R2.CraftWorld;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class RemoveOldDamagedBlocksTask extends BukkitRunnable {
